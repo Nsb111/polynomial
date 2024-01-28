ad632e9b2cb8ec12e4e7bfcc821e8c1818043846
Naif Albalawi <nsb1@bu.edu>
2024-01-28 17:46:50 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -37,3 +37,30 @@ class Mul:
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+
+
+#excercise B
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        return repr(self.p1) + " - " + repr(self.p2)
+
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        if p2 == 0:
+            raise ValueError("Division by zero")
+        
+    def __repr__(self):
+        return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+
+
+poly = Add( Sub( Int(4), Int(3)), Sub( X(), Div( Int(1), Add( Mul(X(), X()), Int(1)))))
+print(poly)
+
+    
+    
