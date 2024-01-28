c6ea06c0eede3020d066e1036f83ae602fdff3dd
Naif Albalawi <nsb1@bu.edu>
2024-01-28 17:58:45 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate (self, x):
+        return x
 
 class Int:
     def __init__(self, i):
@@ -11,6 +14,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate (self, x):         #Eval
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -19,6 +25,9 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate (self, x):         #Eval
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -33,7 +42,9 @@ class Mul:
         if isinstance(self.p2, Add):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
-
+    
+    def evaluate (self, x):         #Eval
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
@@ -47,6 +58,9 @@ class Sub:
     
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
+    
+    def evaluate (self, x):           #Eval
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
 
 class Div:
     def __init__(self, p1, p2):
@@ -57,10 +71,15 @@ class Div:
         
     def __repr__(self):
         return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
-
+    
+    def evaluate (self, x):             #Eval
+        denominator = self.p2.evaluate(x)
+        if denominator == 0:
+            raise ValueError("Division by zero")
+        return self.p1.evaluate(x) / denominator
 
 poly = Add( Sub( Int(4), Int(3)), Sub( X(), Div( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
 
-    
+
     
